bf23c790582971471d39086bdc83906fa26d6297
Mohammad Ra'ed <50214064+mohammadalqaisyyy@users.noreply.github.com>
2022-03-25 12:22:08 +0300

    First commit

---
+++ Task_7/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿
+namespace Task_7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.gif = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.move = new System.Windows.Forms.Timer(this.components);
+            this.moveShark = new System.Windows.Forms.Timer(this.components);
+            this.randomMove = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.win = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Wheat;
+            this.label1.Location = new System.Drawing.Point(1162, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 50);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "100";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // gif
+            // 
+            this.gif.Enabled = true;
+            this.gif.Interval = 1;
+            this.gif.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.panel1.Location = new System.Drawing.Point(549, 254);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(140, 77);
+            this.panel1.TabIndex = 0;
+            // 
+            // move
+            // 
+            this.move.Enabled = true;
+            this.move.Interval = 20;
+            this.move.Tick += new System.EventHandler(this.moveTimer);
+            // 
+            // moveShark
+            // 
+            this.moveShark.Enabled = true;
+            this.moveShark.Interval = 3000;
+            this.moveShark.Tick += new System.EventHandler(this.moveShark_Tick);
+            // 
+            // randomMove
+            // 
+            this.randomMove.Enabled = true;
+            this.randomMove.Interval = 30000;
+            this.randomMove.Tick += new System.EventHandler(this.randomMove_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(1156, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 40);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Score";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // win
+            // 
+            this.win.Enabled = true;
+            this.win.Interval = 120000;
+            this.win.Tick += new System.EventHandler(this.win_Tick);
+            // 
+            // button1
+            // 
+            this.button1.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(1118, 508);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 41);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Replay";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1258, 561);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Sharks game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer gif;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer move;
+        private System.Windows.Forms.Timer moveShark;
+        private System.Windows.Forms.Timer randomMove;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer win;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Task_7/Form1.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Mohammad Al-Qaisy
+
+namespace Task_7
+{
+    public partial class Form1 : Form
+    {
+        private bool up, down, left, right;
+        private int speed = 5, score = 100, _height, _width;
+        private List<Panel> list = new List<Panel>();
+        private List<bool> _in = new List<bool>();
+        Random random = new Random(System.DateTime.Now.Millisecond);
+
+        private GifImage gifImage = null;
+        private string filePath = "diver3.gif";
+        public Form1()
+        {
+            InitializeComponent();
+            gifImage = new GifImage(filePath);
+            gifImage.ReverseAtEnd = false;
+            panel1.Location = new Point(0, 0);
+            label1.Text = score.ToString();
+            label1.Left = this.Width - 100;
+            label2.Left = this.Width - 100;
+            button1.Left = this.Width - button1.Width - 20;
+            button1.Top = this.Height - button1.Height - 50;
+            _height = this.Height;
+            _width = this.Width;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                list.Add(shark(i, 0));
+                this.Controls.Add(list[i]);
+                _in.Add(false);
+            }
+        }
+
+        Panel shark(int l,int add)
+        {
+            Panel x = new Panel();
+            x.BackColor = Color.Transparent;
+            x.BackgroundImageLayout = ImageLayout.Stretch;
+            x.BackgroundImage = Properties.Resources.shark;
+            x.Size= new System.Drawing.Size(220, 80);
+            x.Location = new Point(random.Next(0, this.Width - x.Width),
+                    random.Next(0, this.Height - x.Height));
+            return x;
+        }
+
+        private void moveShark_Tick(object sender, EventArgs e)
+        {
+            reset();
+            for (int i = 0; i < 5; i++)
+            {
+                list[i].Left -= 5;
+                if (list[i].Left + list[i].Width <= 0)
+                    list[i].Left = this.Width;
+                this.Controls.Add(list[i]);
+            }
+            decScore();
+        }
+
+        void reset()
+        {
+            for (int i = 0; i < 5; i++)
+                this.Controls.Remove(list[i]);
+        }
+
+        private void win_Tick(object sender, EventArgs e)
+        {
+            up = false; down = false; right = false; left = false;
+            randomMove.Enabled = false; move.Enabled = false; win.Enabled = false;
+            moveShark.Enabled = false; gif.Enabled = false;
+            if (score > 0)
+                MessageBox.Show("You Win");
+            else
+                MessageBox.Show("Game over");
+
+            replay();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            replay();
+        }
+
+        void replay()
+        {
+            panel1.Location = new Point(0, 0);
+            up = false;down = false;right = false;left = false;
+            reset();
+            for (int i = 0; i < 5; i++)
+            {
+                list[i].Location = new Point(random.Next(0, this.Width - list[i].Width),
+                    random.Next(0, this.Height - list[i].Height));
+                this.Controls.Add(list[i]);
+            }
+            randomMove.Enabled = true; move.Enabled = true; win.Enabled = true;
+            moveShark.Enabled = true; gif.Enabled = true;
+            score = 100;
+            label1.Text = score.ToString();
+        }
+
+        private void randomMove_Tick(object sender, EventArgs e)
+        {
+            reset(); 
+            for (int i = 0; i < 5; i++)
+            {
+                list[i].Location = new Point(random.Next(0, this.Width-list[i].Width),
+                    random.Next(0, this.Height-list[i].Height));
+                this.Controls.Add(list[i]);
+            }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            label1.Left = this.Width - 100;
+            label2.Left = this.Width - 100;
+            button1.Left = this.Width - button1.Width - 20;
+            button1.Top = this.Height - button1.Height - 50;
+            double x = 0, y = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                if (_width == 0 || _height == 0)
+                    break;
+                x = (list[i].Location.X * 1.0 / _width) * this.Width;
+                y = (list[i].Location.Y *1.0 / _height) * this.Height;
+                list[i].Location = new Point((int)x, (int)y);
+                this.Controls.Add(list[i]);
+            }
+            x = (panel1.Location.X * 1.0 / _width) * this.Width;
+            y = (panel1.Location.Y * 1.0 / _height) * this.Height;
+            panel1.Location = new Point((int)x, (int)y);
+            _width = this.Width;
+            _height = this.Height;
+        }
+
+        void decScore()
+        {
+            int xLeftPoint = panel1.Location.X, xRightPoint = panel1.Location.X + panel1.Width,
+                    yTopPoint = panel1.Location.Y, yDownPoint = panel1.Location.Y + panel1.Height;
+            int pLx, pRx, pTy, pDy;
+            foreach (Panel t in list)
+            {
+                pLx = t.Location.X; pRx = t.Location.X + t.Width;
+                pTy = t.Location.Y; pDy = t.Location.Y + t.Height;
+                if (((pLx <= xLeftPoint && xLeftPoint <= pRx) || (pLx <= xRightPoint && xRightPoint <= pRx)) &&
+                    ((pTy <= yTopPoint && yTopPoint <= pDy) || (pTy <= yDownPoint && yDownPoint <= pDy)))
+                {
+                    if (_in[list.IndexOf(t)] == false)
+                    {
+                        _in[list.IndexOf(t)] = true;
+                        score -= 10;
+                        label1.Text = score.ToString();
+                        if (score == 0)
+                        {
+                            up = false; down = false; right = false; left = false;
+                            randomMove.Enabled = false; move.Enabled = false; win.Enabled = false;
+                            moveShark.Enabled = false; gif.Enabled = false;
+                            MessageBox.Show("Game over");
+                            replay();
+                        }
+                    }
+                }
+                else {
+                    _in[list.IndexOf(t)] = false;
+                }
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+                up = true;
+            if (e.KeyCode == Keys.Down)
+                down = true;
+            if (e.KeyCode == Keys.Left)
+                left = true;
+            if (e.KeyCode == Keys.Right)
+                right = true;
+            if (panel1.Left+ panel1.Width < 0)
+                panel1.Location = new Point(this.Width, panel1.Top);
+            if (panel1.Left > this.Width)
+                panel1.Location = new Point(-panel1.Width, panel1.Top);
+            if (panel1.Top + panel1.Height < 0)
+                panel1.Location = new Point(panel1.Left, this.Height-50);
+            if (panel1.Top > this.Height)
+                panel1.Location = new Point(panel1.Left, -panel1.Height);
+            decScore();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            panel1.BackgroundImage = gifImage.GetNextFrame();
+        }
+
+        private void moveTimer(object sender, EventArgs e)
+        {
+            if (left)
+                panel1.Left -= speed;
+            if (right)
+                panel1.Left += speed;
+            if (up)
+                panel1.Top -= speed;
+            if (down)
+                panel1.Top += speed;
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+                up = false;
+            if (e.KeyCode == Keys.Down)
+                down = false;
+            if (e.KeyCode == Keys.Left)
+                left = false;
+            if (e.KeyCode == Keys.Right)
+                right = false;
+            decScore();
+        }
+    }
+}
